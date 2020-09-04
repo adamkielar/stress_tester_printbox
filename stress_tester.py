117958e91575359b7bff3dbf5a7b6d16476770fe
Adam Kielar <adam@adamkielar.pl>
2020-09-04 18:43:10 +0200

    Added basic connection logic from client to server and receive response

--- stress_tester.py
+++ stress_tester.py
@@ -1,17 +1,27 @@
 import argparse
-import time
+import socket
+import urllib.parse
+from http.client import HTTPConnection
 
 
-
-def test_server(request_number: int, connection_number: int, number: int):
-    pass
+def test_server(number: int, request_number: int, ):
+    params = urllib.parse.urlencode({'number': number, 'perf': request_number})
+    path = f'/prime?{params}'
+    conn = HTTPConnection('localhost', 8080)
+    conn.request('GET', path)
+    response = conn.getresponse()
+    print(response.status, response.reason, response.msg)
+    print(response.read())
+    conn.close()
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Started testing server.')
-    parser.add_argument('-r', '--request_number', default=1,
-                        help='number of server requests', type=int)
-    parser.add_argument('-c', '--connection_number', default=1,
-                        help='number of simultaneous connections', type=int)
     parser.add_argument('-n', '--number', default=1000000000001,
                         help='prime number', type=int)
+    parser.add_argument('-r', '--request_number', default=1,
+                        help='number of request', type=int)
+    parser.add_argument('-t', '--thread_number', default=1,
+                        help='number of connections', type=int)
+    args = parser.parse_args()
+    test_server(args.number, args.request_number)
